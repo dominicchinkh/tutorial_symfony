db4f31a8a3a80e9504d3e38d79be1b386d27d10c
Dominic Chin <dominicchinkh@gmail.com>
2026-07-18 15:43:57 +1000

    Twig component as form

---
+++ symfony/src/Twig/Components/ProductForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class ProductForm extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    /**
+     * The initial data used to create the form.
+     */
+    #[LiveProp]
+    public ?Product $initialFormData = null;
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(ProductType::class, $this->initialFormData);
+    }
+
+    #[LiveAction]
+    public function save(EntityManagerInterface $entityManager)
+    {
+        // Submit the form. If validation fails, an exception is thrown
+        // and the component is automatically re-rendered with the errors
+        $this->submitForm();
+
+        /** @var Product $product */
+        $product = $this->getForm()->getData();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'product saved!');
+
+        return $this->redirectToRoute('template-component', [
+        ]);
+    }
+}
