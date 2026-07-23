04946a46e3fa3cc1b8bd6420d3b933130fb286d8
dominic <dominicchinkh@gmail.com>
2026-07-23 13:45:47 +1000

    Customize query parameter name. Query parameter validation

--- symfony/src/Twig/Components/SearchModule.php
+++ symfony/src/Twig/Components/SearchModule.php
@@ -3,17 +3,38 @@
 namespace App\Twig\Components;
 
 use App\Dto\SearchFilters;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Metadata\UrlMapping;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
 use Symfony\UX\TwigComponent\Attribute\PostMount;
 
 #[AsLiveComponent]
 class SearchModule
 {
     use DefaultActionTrait;
+    use ValidatableComponentTrait;
+
+    /*
+     * You can use the as option in your LiveProp definition
+     *   #[LiveProp(writable: true, url: new UrlMapping(as: 'q'))]
+     *
+     * The query value will appear in the URL like https://my.domain/search?q=my+query+string
+     * 
+     */
+
+    /*
+     * If you need to change the parameter name on a specific page, you can leverage the modifier option
+     *   #[LiveProp(writable: true, url: true, modifier: 'modifyQueryProp')]
+     * 
+     * In Twig template:
+     *   <twig:SearchModule alias="q" />
+    */
 
     #[LiveProp(writable: true, url: true)]
+    #[Assert\NotBlank]
     public string $query = '';
 
     /** @var string[] */
@@ -23,10 +44,18 @@ class SearchModule
     #[LiveProp(writable: ['category', 'minPrice'], url: true)]
     public SearchFilters $filters;
 
+    #[LiveProp]
+    public ?string $alias = null;
+
     #[PostMount]
     public function postMount(): void
     {
         $this->filters ??= new SearchFilters();
+
+        // Validate 'query' field without throwing an exception, so the component can
+        // be mounted anyway and a validation error can be shown to the user
+
+        $this->validateField('query', false);
     }
 
     /** @return string[] */
@@ -43,5 +72,13 @@ class SearchModule
             'books' => 'Books',
             'electronics' => 'Electronics',
         ];
+    }
+
+    public function modifyQueryProp(LiveProp $liveProp): LiveProp
+    {
+        if ($this->alias) {
+            $liveProp = $liveProp->withUrl(new UrlMapping(as: $this->alias));
+        }
+        return $liveProp;
     }
 }
