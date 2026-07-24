e337f7bdf0809b598719c1f1abc117b3142a76c7
dominic <dominicchinkh@gmail.com>
2026-07-24 11:35:06 +1000

    Add comment

--- symfony/src/Twig/Components/TodoFooter.php
+++ symfony/src/Twig/Components/TodoFooter.php
@@ -13,7 +13,9 @@ class TodoFooter
 
     // With `updateFromParent`, when the parent component re-renders, if the value of the count prop changes, the child 
     // will make a second Ajax request to re-render itself
-    
+
     #[LiveProp(updateFromParent: true)]
     public int $count = 0;
+
+    // Note: actions in a child do not affect the parent
 }
