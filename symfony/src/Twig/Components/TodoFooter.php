bf1654d082db198bd83f1b979fb94cc5b617bfdd
dominic <dominicchinkh@gmail.com>
2026-07-27 16:03:02 +1000

    Updating a parent model from a child

--- symfony/src/Twig/Components/TodoFooter.php
+++ symfony/src/Twig/Components/TodoFooter.php
@@ -3,19 +3,30 @@
 namespace App\Twig\Components;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 class TodoFooter
 {
     use DefaultActionTrait;
 
-    // With `updateFromParent`, when the parent component re-renders, if the value of the count prop changes, the child 
-    // will make a second Ajax request to re-render itself
-
+    // Re-renders when the parent passes a new count (e.g. after adding a todo).
     #[LiveProp(updateFromParent: true)]
     public int $count = 0;
 
-    // Note: actions in a child do not affect the parent
+    // Writable in the child; synced to the parent's listName via dataModel.
+    #[LiveProp(writable: true)]
+    public string $listName = '';
+
+    // Writable prop that survives count-driven child re-renders (not updateFromParent).
+    #[LiveProp(writable: true)]
+    public bool $isVisible = true;
+
+    #[LiveAction]
+    public function toggleVisibility(): void
+    {
+        $this->isVisible = !$this->isVisible;
+    }
 }
