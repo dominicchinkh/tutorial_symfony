76e1372db65a64693f15c621eddaba406748fcc0
dominic <dominicchinkh@gmail.com>
2026-08-04 16:05:00 +1000

    More template rendering and debugging

--- symfony/src/Controller/TemplateController.php
+++ symfony/src/Controller/TemplateController.php
@@ -6,23 +6,44 @@ use App\Dto\Item;
 use App\Dto\Notification;
 use App\Dto\SearchFilters;
 use App\Repository\ProductRepository;
+use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 #[Route('/template', name: 'template-')]
 final class TemplateController extends AbstractController
 {
     public function __construct(
         private readonly ProductRepository $productRepository,
+        private readonly Environment $twig,
     ) {}
 
     #[Route('/notification', name: 'notification', methods: ['GET'])]
     public function notification(): Response
     {
-        // get the user information and notifications somehow
+        // Get the user information and notifications somehow
         $userFirstName = 'Dominic';
         $userNotifications = ['PR ready', 'PR approved'];
+
+        /*
+         *  The `renderBlock()` method returns a `Response` object with the
+         *  block contents
+         * 
+         *    return $this->renderBlock('template/user/notification.html.twig', 'page_contents', [
+         *        // ...
+         *    ]);
+         */
+
+        /*
+         *  The `renderBlockView()` method only returns the contents created by the template block, 
+         *  so you can use those contents later in a `Response` object
+         * 
+         *    $contents = $this->renderBlockView('template/user/notification.html.twig', 'page_contents', [
+         *        // ...
+         *    ]);
+         */
 
         // The template path is the relative file path from `templates/`
         return $this->render('template/user/notification.html.twig', [
@@ -34,6 +55,60 @@ final class TemplateController extends AbstractController
             'user_first_name' => $userFirstName,
             'notifications' => $userNotifications,
         ]);
+    }
+
+    /*
+     *  Another option is to use the #[Template] attribute on the controller method to define 
+     *  the template to render:
+     * 
+     *  You can use the #[Template] attribute on the controller to specify a block to render
+     * 
+     *    #[Template('template/user/notification.html.twig', block: 'page_contents')]
+    */
+
+    #[Route('/notification/template-attribute', name: 'notification-template-attribute', methods: ['GET'])]
+    #[Template('template/user/notification.html.twig')]
+    public function notificationTemplateAttribute(): array
+    {
+        $userFirstName = 'Dominic';
+        $userNotifications = ['PR ready', 'PR approved'];
+        
+        // When using the #[Template] attribute, you only need to return
+        // an array with the parameters to pass to the template (the attribute
+        // is the one which will create and return the Response object)
+
+        return [
+           'user_first_name' => $userFirstName,
+           'notifications'   => $userNotifications,
+        ];
+    }
+
+    /*
+     *  Rendering a Template in Services
+     * 
+     *  Inject the twig Symfony service into your own services and use its render() method
+     * 
+     */
+    #[Route('/notification/twig', name: 'notification-twig', methods: ['GET'])]
+    public function notificationTwig(): Response
+    {
+        $userFirstName = 'Dominic';
+        $userNotifications = ['PR ready', 'PR approved'];
+
+        // Templates are loaded in the application using a Twig template loader
+        $loader = $this->twig->getLoader();
+
+        // Checking if a template exists
+        if (!$loader->exists('template/user/notification.html.twig')) {
+            return new Response("<h3>Template template/user/notification.html.twig does not exist</h3>");
+        }
+
+        return new Response(
+            $this->twig->render('template/user/notification.html.twig', [
+                'user_first_name' => $userFirstName,
+                'notifications'   => $userNotifications,
+            ])
+        );
     }
 
     #[Route('/blog', name: 'blog', methods: ['GET'])]
@@ -52,6 +127,28 @@ final class TemplateController extends AbstractController
                     "excerpt" => "Tech leaders race to build stable processors."
                 ]
             ]
+        ]);
+    }
+
+    #[Route('/blog/articles/recent', name: 'latest_articles', methods: ['GET'])]
+    public function recentArticles(int $max = 3): Response
+    {
+        // get the recent articles somehow (e.g. making a database query)
+        $articles = [
+            [
+                "title" => "Ancient Artifacts discovered",
+                "slug" => "unearthing-forgotten-relics",
+                "excerpt" => "Archaeologists uncover deeply buried secrets."
+            ],
+            [
+                "title" => "Future of Quantum Computing",
+                "slug" => "unlocked-quantum-processing-power",
+                "excerpt" => "Tech leaders race to build stable processors."
+            ]
+        ];
+
+        return $this->render('template/blog/_recent_articles.html.twig', [
+            'articles' => $articles
         ]);
     }
 
